5d3d5ba030ad028d2c387641e3344d18d0657c4d
terryt925 <thindterry@yahoo.com>
2021-06-08 16:54:48 -0700

    Add window top

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,15 @@
 	var	$window = $(window),
 		$body = $('body');
 
+	your_email = "thindterry@yahoo.com";
+	emailSub = "personal site";
+	emailBody = document.getElementById("message").innerText; 
+	var email_btn = document.getElementById('send');
+	email_btn.addEventListener("click",function(e){
+		e.preventDefault();
+		location.href = "mailto:"+your_email+'&subject='+emailSub+'&body='+emailBody;
+	});
+
 	// Breakpoints.
 		breakpoints({
 			wide:      [ '1281px',  '1680px' ],
@@ -27,3 +36,4 @@
 		});
 
 })(jQuery);
+
